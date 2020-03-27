ea7cd7829f6f7095f26b55e7b51496aba687e9ed
Lucas Padilha <padilhaght@gmail.com>
2020-03-27 20:41:41 +0100

    Finished sign-in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -6,7 +6,7 @@ import './sign-in.styles.scss';
 // components
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
     constructor() {
@@ -18,10 +18,18 @@ class SignIn extends Component {
         };
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: '' });
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+
+            this.setState({ email: '', password: '' });
+        } catch(error) {
+            console.error(error);
+        }
     };
 
     handleChange = event => {
